1957c411ee810027e5d5b15249b0714a0c7ae4f8
Fabriciovich <fabriciocostadoni@gmail.com>
2023-10-09 10:48:50 -0300

    Se utilizo populate y se hizo la poblacion de datos en ambos controladores

--- src/controllers/author.controller.js
+++ src/controllers/author.controller.js
@@ -1,8 +1,11 @@
 import { AuthorModel } from '../models/author.js';
 
-export const getAuthorsCtrl = async (req, res) => {
+export const getAuthorsCtrl = async (_req, res) => {
     try {
-        const authors = await AuthorModel.find();
+        const authors = await AuthorModel.find().populate({
+            path: 'books',
+            select: 'title gender'
+        });
         res.json(authors);
     } catch (error) {
         res.status(500).json({ message: error.message });
@@ -11,7 +14,10 @@ export const getAuthorsCtrl = async (req, res) => {
 
 export const getAuthorCtrl = async (req, res) => {
     try {
-        const author = await AuthorModel.findById(req.params.id);
+        const author = await AuthorModel.findById(req.params.id).populate({
+            path: 'books',
+            select: 'title gender'
+        });
         res.json(author);
     } catch (error) {
         res.status(500).json({ message: error.message });
--- src/controllers/book.controller.js
+++ src/controllers/book.controller.js
@@ -1,8 +1,12 @@
 import { BooksModel } from '../models/books.js';
+import { AuthorModel } from '../models/author.js';
 
-export const getBooksCtrl = async (req, res) => {
+export const getBooksCtrl = async (_req, res) => {
     try {
-        const books = await BooksModel.find();
+        const books = await BooksModel.find().populate({
+            path: 'author',
+            select: 'name surname'
+        });
         res.json(books);
     } catch (error) {
         res.status(500).json({ message: error.message });
@@ -11,7 +15,10 @@ export const getBooksCtrl = async (req, res) => {
 
 export const getBookCtrl = async (req, res) => {
     try {
-        const book = await BooksModel.findById(req.params.id);
+        const book = await BooksModel.findById(req.params.id).populate({
+            path: 'author',
+            select: 'name surname'
+        });
         res.json(book);
     } catch (error) {
         res.status(500).json({ message: error.message });
@@ -20,9 +27,27 @@ export const getBookCtrl = async (req, res) => {
 
 export const createBookCtrl = async (req, res) => {
     try {
-        const book = new BooksModel(req.body);
-        await book.save();
-        res.json(book);
+        const { title, gender, authorId } = req.body; // Suponiendo que el cuerpo de la solicitud contiene el título y el ID del autor
+
+        const author = await AuthorModel.findById(authorId);
+
+        if (!author) {
+            return res.status(404).json({ message: 'Autor no encontrado' });
+        }
+
+        const newBook = new BooksModel({
+            title,
+            gender,
+            author: author._id, // Establecer la referencia al autor
+        });
+
+        await newBook.save();
+
+        // Agregar el libro al autor
+        author.books.push(newBook);
+        await author.save();
+
+        res.json(newBook);
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
